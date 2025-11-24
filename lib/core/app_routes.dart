92ed53757bf305ba32edcd8076dc6a8298018987
yigitnarci <148265497+yigitnarci@users.noreply.github.com>
2025-11-24 14:25:40 +0300

    Prune to my screens

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,20 +1,6 @@
 class AppRoutes {
-  static const signIn = '/sign-in';
-  static const signUp = '/sign-up';
-  static const terms = '/terms';
-
-  static const home = '/home';
-
   static const globalSearch = '/global-search';
-  static const filterClasses = '/filter-classes';
 
   static const selectFaculty = '/select-faculty';
   static const lecturesList = '/lectures-list';
-  static const rating = '/rating';
-
-  static const comments = '/comments';
-  static const discussions = '/discussions';
-
-  static const profile = '/profile';
 }
-
